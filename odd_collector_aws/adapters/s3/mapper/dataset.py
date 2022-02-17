c235b2d40b0d39226912d6208dc33ae0be2a7ecd
HaykManukyanAvetiky <manukyantt@gmail.com>
2022-02-17 16:29:04 +0400

    new s3 adapter

--- odd_collector_aws/adapters/s3/mapper/dataset.py
+++ odd_collector_aws/adapters/s3/mapper/dataset.py
@@ -2,17 +2,23 @@ from typing import Dict, List, Any
 
 from odd_models.models import DataSetField, Type, DataSetFieldType, DataEntity, DataSet, DataEntityType
 from oddrn_generator.generators import S3Generator
-from pyarrow import Field, Schema
+from pyarrow import Field, Schema, lib
+
+SCHEMA_FILE_URL = "https://raw.githubusercontent.com/opendatadiscovery/opendatadiscovery-specification/" \
+                  "main/specification/extensions/s3.json"
 
 TYPE_MAP: Dict[str, Type] = {
     'int8': Type.TYPE_INTEGER,
     'int16': Type.TYPE_INTEGER,
+    'int32': Type.TYPE_INTEGER,
     'int64': Type.TYPE_INTEGER,
     'uint8': Type.TYPE_INTEGER,
     'uint16': Type.TYPE_INTEGER,
+    'uint32': Type.TYPE_INTEGER,
     'uint64': Type.TYPE_INTEGER,
     'float8': Type.TYPE_NUMBER,
     'float16': Type.TYPE_NUMBER,
+    'float32': Type.TYPE_NUMBER,
     'float64': Type.TYPE_NUMBER,
     'time32': Type.TYPE_TIME,
     'time64': Type.TYPE_TIME,
@@ -28,25 +34,30 @@ TYPE_MAP: Dict[str, Type] = {
     'large_string': Type.TYPE_STRING,
     'large_utf8': Type.TYPE_STRING,
     'decimal128': Type.TYPE_NUMBER,
+    lib.ListType: Type.TYPE_LIST,
+    lib.StructType: Type.TYPE_STRUCT,
 }
 
 
-def map_dataset(file: Dict[str, Any], schema: Schema, oddrn_gen: S3Generator) -> DataEntity:
-    name = file["Key"].replace('/', ':').replace('.parquet', '')
+def map_dataset(name, schema: Schema, metadata: Dict, oddrn_gen: S3Generator) -> DataEntity:
+    name = ':'.join(name.split('/')[1:])
 
     oddrn_gen.set_oddrn_paths(keys=name)
+    rows = metadata['Rows']
+    del metadata['Rows']
+    metadata = [{'schema_url': f'{SCHEMA_FILE_URL}#/definitions/GlueDataSetExtension',
+                'metadata': metadata}]
 
     return DataEntity(
         name=name,
         oddrn=oddrn_gen.get_oddrn_by_path('keys', name),
-        metadata=[],
+        metadata=metadata,
         # TODO
         updated_at=None,
         created_at=None,
         type=DataEntityType.FILE,
         dataset=DataSet(
-            # TODO
-            rows_number=None,
+            rows_number=rows,
             field_list=map_columns(schema, oddrn_gen)
         )
     )
@@ -57,7 +68,7 @@ def map_column(field: Field, oddrn_gen: S3Generator) -> DataSetField:
         name=field.name,
         oddrn=oddrn_gen.get_oddrn_by_path('columns', field.name),
         type=DataSetFieldType(
-            type=TYPE_MAP.get(str(field.type), Type.TYPE_UNKNOWN),
+            type=TYPE_MAP.get(str(field.type), TYPE_MAP.get(type(field.type), Type.TYPE_UNKNOWN)),
             logical_type=str(field.type),
             is_nullable=field.nullable
         )
