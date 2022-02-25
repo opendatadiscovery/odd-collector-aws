e9ad61d8aed5287da44034e0feeca62b8f8bb853
HaykManukyanAvetiky <manukyantt@gmail.com>
2022-02-25 11:59:32 +0400

    Adding metadata to sqs

--- odd_collector_aws/adapters/sqs/adapter.py
+++ odd_collector_aws/adapters/sqs/adapter.py
@@ -4,7 +4,10 @@ from .sqs_generator import SqsGenerator
 from odd_models.models import DataEntityList, DataEntity, DataEntityType
 from typing import Any, Dict, List
 from odd_collector_aws.domain.plugin import SQSPlugin
+import datetime
 
+SCHEMA_FILE_URL="https://raw.githubusercontent.com/opendatadiscovery/opendatadiscovery-specification/" \
+                "main/specification/extensions/sqs.json"
 
 
 class SqsAdapter:
@@ -36,12 +39,23 @@ class SqsAdapter:
             sqs_queues = []
             for queue in self._sqs_client.list_queues()['QueueUrls']:
                 queue_name = queue.split('/')[-1]
+                queue_attributes = self._sqs_client.get_queue_attributes(QueueUrl=queue, AttributeNames=['All'])['Attributes']
+                created_at=datetime.datetime.fromtimestamp(int(queue_attributes['CreatedTimestamp']))
+                updated_at=datetime.datetime.fromtimestamp(int(queue_attributes['LastModifiedTimestamp']))
+                del queue_attributes['CreatedTimestamp']
+                del queue_attributes['LastModifiedTimestamp']
+                del queue_attributes['Policy']
+                queue_attributes['AccountID'] = self._account_id
+                metadata = [{'schema_url': f'{SCHEMA_FILE_URL}#/definitions/SQSDataSetExtension',
+                'metadata': queue_attributes}]
                 sqs_queues.append(
                     DataEntity(
                         name = queue_name,
                         oddrn=self.__oddrn_generator.get_oddrn_by_path('queue', queue_name),
-                        type=DataEntityType.UNKNOWN
-
+                        type=DataEntityType.UNKNOWN,
+                        created_at=created_at,
+                        updated_at=updated_at,
+                        metadata=metadata
                     )
                 )
         except ClientError:
