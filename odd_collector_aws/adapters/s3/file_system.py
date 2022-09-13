2e7c7f31cea875e50bd7c25dbf99f7459ed4868f
Pavel Makarichev <vixtir90@gmail.com>
2022-09-13 02:25:50 -0700

    chore(s3): bump s3 generator version (#16)

--- odd_collector_aws/adapters/s3/file_system.py
+++ odd_collector_aws/adapters/s3/file_system.py
@@ -1,16 +1,19 @@
 import pyarrow.dataset as ds
 from pyarrow.fs import S3FileSystem
 
+from odd_collector_aws.domain.plugin import AwsPlugin
+
 
 class FileSystem:
     """
     FileSystem hides pyarrow.fs implementation details.
     """
 
-    def __init__(self, config):
+    def __init__(self, config: AwsPlugin):
         self.__fs = S3FileSystem(
             access_key=config.aws_access_key_id,
             secret_key=config.aws_secret_access_key,
+            session_token=config.aws_session_token,
             region=config.aws_region,
             endpoint_override=config.endpoint_url,
         )
--- odd_collector_aws/adapters/s3/strategies/select_strategy.py
+++ odd_collector_aws/adapters/s3/strategies/select_strategy.py
@@ -1,12 +1,6 @@
-from odd_collector_aws.adapters.s3.strategies import (
-    EachFileStrategy,
-    FolderStrategy,
-)
+from odd_collector_aws.adapters.s3.strategies import EachFileStrategy, FolderStrategy
 from odd_collector_aws.domain.dataset_config import DatasetConfig
 
 
 def select_strategy(dataset_config: DatasetConfig):
-    if dataset_config.folder_as_dataset:
-        return FolderStrategy
-
-    return EachFileStrategy
+    return FolderStrategy if dataset_config.folder_as_dataset else EachFileStrategy
