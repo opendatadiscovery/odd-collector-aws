bbac672a377e3c1843979cd7de9efaddd2b9ecd3
Pavel Mackarichev <vixtir90@gmail.com>
2023-04-20 10:58:28 +0300

    chore: remove redundant try/except block

--- odd_collector_aws/__main__.py
+++ odd_collector_aws/__main__.py
@@ -1,20 +1,14 @@
-import traceback
 from pathlib import Path
 
 from odd_collector_sdk.collector import Collector
-from odd_collector_sdk.logger import logger
 from odd_collector_aws.domain.plugin import PLUGIN_FACTORY
 
 COLLECTOR_PACKAGE = __package__
-CONFIG_PATH= Path().cwd() / "collector_config.yaml"
+CONFIG_PATH = Path().cwd() / "collector_config.yaml"
 
-try:
-    collector = Collector(
-        config_path=CONFIG_PATH,
-        root_package=COLLECTOR_PACKAGE,
-        plugin_factory=PLUGIN_FACTORY,
-    )
-    collector.run()
-except Exception as e:
-    logger.debug(traceback.format_exc())
-    logger.error(e)
+collector = Collector(
+    config_path=CONFIG_PATH,
+    root_package=COLLECTOR_PACKAGE,
+    plugin_factory=PLUGIN_FACTORY,
+)
+collector.run()
